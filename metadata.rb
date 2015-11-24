3eca50b873b1951433728f85cc53e895bd60eea0
Joshua Thornton <joshua@techinasia.com>
2015-11-24 10:53:07 +0800

    Skeleton.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'nginx-site'
+maintainer 'Joshua Thornton'
+maintainer_email 'jshthornton@gmail.com'
+license 'MIT'
+description 'Manages nginx sites'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
